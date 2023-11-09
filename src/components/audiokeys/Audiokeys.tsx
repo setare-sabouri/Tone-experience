1b7de9a4e4112eedb4c0bd2e5900b1120430dddb
Setareh <setarehsaboory@gmail.com>
2023-11-09 15:17:46 +0100

    fine steps

---
+++ src/components/audiokeys/Audiokeys.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState, useRef } from "react";
+
+// import * as AudioKeys from "audiokeys";
+
+interface Audiokeys {}
+
+const Audiokeys: React.FC<Audiokeys> = (props) => {
+  // const audokeys = new AudioKeys();
+  // console.log(audokeys);
+  return (
+    <>
+      <p>audiokeys</p>
+    </>
+  );
+};
+
+export default Audiokeys;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import Experience from "@components/experience/ToneExperience";
 import RootLayout from "@components/layout/layout";
+import Audiokeys from "@components/audiokeys/Audiokeys";
 const Home: NextPage = () => {
   return (
     <>
       <RootLayout>
-        <Experience></Experience>
+        <Experience />
       </RootLayout>
     </>
   );
