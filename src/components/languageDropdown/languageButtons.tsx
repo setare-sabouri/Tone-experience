70cabaea1705685ae7b58f3519d99ed3fc5638cf
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-01-03 08:13:29 +0100

    dropdown input for language

---
+++ src/components/languageDropdown/languageButtons.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import styles from './languageButtons.module.scss';
+
+const LanguageDropdown: React.FC = () => {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <select
+        onChange={(e) => changeLanguage(e.target.value)}
+        value={i18n.language}
+      >
+        <option value="en">English</option>
+        <option value="sv">Swedish</option>
+      </select>
+    </div>
+  );
+};
+
+export default LanguageDropdown;
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next'; // Updated import
 import styles from './layout.module.scss';
-
+import LanguageDropdown from '@components/languageDropdown/languageButtons';
 export default function RootLayout({
   children,
 }: {
@@ -12,6 +12,7 @@ export default function RootLayout({
     <>
       <nav className={styles.title}>
         <p>{t('welcome')}</p>
+        <LanguageDropdown />
       </nav>
       <main>{children}</main>
       {/* rest of the page  */}
