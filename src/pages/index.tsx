a02438ac943d3c1c73fc053cbebd21aca6ee67b7
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-01-06 06:04:27 +0100

    models

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,16 @@ import type { NextPage } from 'next';
 import Experience from '@components/experience/ToneExperience';
 import RootLayout from '@components/layout/layout';
 import Audiokeys from '@components/audiokeys/Audiokeys';
+import Cube from '@components/cubeModel/cube';
+
+const modelUrl = '/models/nike.glb';
 
 const Home: NextPage = () => {
   return (
     <>
       <RootLayout>
         <Experience />
+        <Cube modelUrl={modelUrl} />
       </RootLayout>
     </>
   );
