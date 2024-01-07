6b922f7178b69a21e2a4cd817c1cef648068db47
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-01-07 22:34:03 +0100

    style updates on canvas

--- src/components/GlobalCanvas/Gcanvas.tsx
+++ src/components/GlobalCanvas/Gcanvas.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-
+import styles from './Canvas.module.scss';
 import Cube from '@components/cubeModel/Cube';
 import { Canvas } from 'react-three-fiber';
-import {
-  useGLTF,
-  OrbitControls,
-  Environment,
-  ContactShadows,
-} from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 
 import { Suspense } from 'react';
 const Gcanvas: React.FC = () => {
   return (
-    <>
-      <Canvas shadows camera={{ position: [2, 0, 0] }}>
+    <div className={styles.canvasCnt}>
+      <Canvas shadows camera={{ position: [5, 0, 0] }}>
         <spotLight
           intensity={0.4}
           angle={0.2}
@@ -23,30 +18,11 @@ const Gcanvas: React.FC = () => {
         />
         <OrbitControls enableZoom={false} />
         <ambientLight intensity={1} />
-        <ContactShadows
-          position={[0, -0.6, 0]}
-          opacity={0.5}
-          scale={8}
-          blur={2}
-          far={2}
-          color={'#FFEF32'}
-        />{' '}
-        {/*yellow*/}
-        <ContactShadows
-          position={[0, -0.6, 0]}
-          opacity={0.7}
-          scale={8}
-          blur={3}
-          far={0.9}
-          color={'#120099'}
-        />{' '}
-        {/* blue */}
-        <directionalLight castShadow color={'red'} far={5} />
         <Suspense fallback={null}>
           <Cube />
         </Suspense>
       </Canvas>
-    </>
+    </div>
   );
 };
 
