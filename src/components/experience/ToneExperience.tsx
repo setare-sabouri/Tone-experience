7517bdc1dfe0423fa96d43818f1f93ac040f8bbe
Setareh <setarehsaboory@gmail.com>
2024-01-08 08:55:41 +0100

    strict mode wrapped

--- src/components/experience/ToneExperience.tsx
+++ src/components/experience/ToneExperience.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-import * as Tone from 'tone';
-import styles from './Experience.module.scss';
-import audioKeys from 'audiokeys';
-import GridContainer from '@components/gridContainer/GridContainer';
-import keysnotes from '@utils/notes';
+import React, { useEffect, useRef } from "react";
+import * as Tone from "tone";
+import styles from "./Experience.module.scss";
+import audioKeys from "audiokeys";
+import GridContainer from "@components/gridContainer/GridContainer";
+import keysnotes from "@utils/notes";
 interface ExperienceProps {}
 
 const Experience: React.FC<ExperienceProps> = (props) => {
@@ -21,7 +21,7 @@ const Experience: React.FC<ExperienceProps> = (props) => {
 
   const playNote = (note: string) => {
     if (synthRef.current) {
-      synthRef.current.triggerAttackRelease(note, '8n');
+      synthRef.current.triggerAttackRelease(note, "8n");
     }
   };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-import '@styles/globals.css';
-import type { AppProps } from 'next/app';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
-
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n";
+import { StrictMode } from "react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <I18nextProvider i18n={i18n}>
-      <Component {...pageProps} />
-    </I18nextProvider>
+    <StrictMode>
+      <I18nextProvider i18n={i18n}>
+        <Component {...pageProps} />
+      </I18nextProvider>
+    </StrictMode>
   );
 }
