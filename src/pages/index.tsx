52caf3e40cde0bcf387dd32a1d2d25f3c33f0dad
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-04-05 06:18:16 +0200

    removed unneccssaarryy cpms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import type { NextPage } from 'next';
 import Experience from '@components/experience/ToneExperience';
 import RootLayout from '@components/layout/layout';
-import Audiokeys from '@components/audiokeys/Audiokeys';
-import Gcanvas from '@components/GlobalCanvas/Gcanvas';
+
 const Home: NextPage = () => {
   return (
     <>
       <RootLayout>
         <Experience />
-        {/* <Gcanvas /> */}
       </RootLayout>
     </>
   );
