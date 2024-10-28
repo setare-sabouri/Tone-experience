6db7f1fbb25ff2de9527e9b69b69bdf542211529
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-10-28 21:43:25 +0100

    back after 6m

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next'; // Updated import
 import styles from './layout.module.scss';
 import LanguageDropdown from '@components/languageDropdown/languageButtons';
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode; }) {
   const { t } = useTranslation();
   return (
     <div className={styles.layoutwrapp}>
