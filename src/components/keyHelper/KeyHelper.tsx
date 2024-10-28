ba636cc5d4cbc29316eda32da4b1fe1877cf92fa
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-10-28 22:45:06 +0100

    help btn i18

--- src/components/keyHelper/KeyHelper.tsx
+++ src/components/keyHelper/KeyHelper.tsx
@@ -3,11 +3,12 @@ import React, { useState } from 'react';
 import styles from './KeyHelper.module.scss';
 import GuideLayout from '@components/guideLayout/GuideLayout';
 import steps from '@utils/guideSteps';
-interface KeyHelperProps {}
+import { useTranslation } from 'react-i18next';
+interface KeyHelperProps { }
 
-const KeyHelper: React.FC<KeyHelperProps> = ({}) => {
+const KeyHelper: React.FC<KeyHelperProps> = ({ }) => {
   const [showGuide, setShowGuide] = useState(false);
-
+  const { t } = useTranslation();
   const toggleKeyboard = () => {
     setShowGuide((prevShowGuide) => !prevShowGuide);
   };
@@ -16,7 +17,7 @@ const KeyHelper: React.FC<KeyHelperProps> = ({}) => {
     <div className={styles.helper}>
       <div>
         <button className={styles.helperBtn} onClick={toggleKeyboard}>
-          Help
+          {t('help')}
         </button>
       </div>
       {showGuide && <GuideLayout steps={steps} />}
