072d682dbb584d1bd35f999ece36d54a8fc0c7c6
setareh <setarehsaboory@gmail.com>
2025-07-30 16:39:46 +0200

    bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@styles/globals.scss";
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../utils/i18n";
