79fcf5172f27b6e7d5ce597491420047a86cc6a6
setareh <setarehsaboory@gmail.com>
2025-07-31 00:28:06 +0200

    bug

--- src/components/SectionOne/experience/Experience.tsx
+++ src/components/SectionOne/experience/Experience.tsx
@@ -4,7 +4,7 @@ import styles from "./Experience.module.scss";
 import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
 
-const Experience = () => {
+const Experience: React.FC = () => {
   return (
     <div className={styles.experienceContainer}>
       <Canvas camera={{ position: [0, 3, 10], fov: 50 }} shadows>
