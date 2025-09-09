9a052f7bccdcfaaeab5c1e17ffedcf2d96d79e56
setareh <setarehsaboory@gmail.com>
2025-09-09 05:06:47 +0200

    undo

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,23 +3,12 @@ import styles from "./Experience.module.scss";
 import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { useEffect } from "react";
+
 
 const Experience = () => {
-
-  useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-    setVh();
-    window.addEventListener("resize", setVh);
-    return () => window.removeEventListener("resize", setVh);
-  }, []);
-
   return (
     <div className={styles.experienceContainer}>
-      <Canvas className={styles.CanvasCls} >
+      <Canvas>
         {/* <Perf/> */}
         <color args={["#1e703b"]} attach={"background"} />
         <Environment preset="city" />
